21e7ecb924e81ee0144d1ab6613ce04abf9a632b
BSchlager009 <bixbyasof2007@gmail.com>
2025-09-03 15:26:21 +0000

    Added BMI index calculation.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,14 @@
 using namespace std;
 
 int main() {
- 
-
-
+    //Declare values
+    const int height = 61;
+    const int weight = 130;
+    //Convert Units
+    double kg = weight*0.453592;
+    double m = height*0.0254;
+    //Calc BMI
+    double BMI = kg/(m*m);
+    //Print :D
+    cout<<"The BMI for this person is : "<<BMI<<'.'<<endl;
 }
